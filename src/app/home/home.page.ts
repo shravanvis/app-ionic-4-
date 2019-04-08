0bfe445b37c4665fce6e040f067af6a030e7dc4d
shravanvis <shravanvishwakarma5@gmail.com>
2019-04-08 23:30:31 +0530

    added ionic camera capacitor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +10,28 @@ import { AuthService } from '../auth.service';
 })
 export class HomePage {
 
-  constructor(private auth: AuthService){ }
+  // image: SafeResourceUrl;
+  image: SafeResourceUrl;
+
+  constructor(private auth: AuthService, private domSanitizer: DomSanitizer){ }
 
   logInWithGoogle(){
     this.auth.login();
   }
   logOutWithGoogle(){
     this.auth.logout()
+  }
+
+  async takePhoto(){
+    const { Camera } = Plugins;
+
+    const result = await Camera.getPhoto({
+      quality: 75,
+      allowEditing: true,
+      source: CameraSource.Camera,
+      resultType: CameraResultType.Base64
+    });
+
+    this.image = this.domSanitizer.bypassSecurityTrustResourceUrl(result && result.base64Data,);
   }
 }
